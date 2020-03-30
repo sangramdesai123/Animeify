e4f418707be88fee7ed9d014efc3b29ec46e4f16
sangramdesai123 <sangram.desai@somaiya.edu>
2020-03-30 21:15:58 +0530

    Angular  setup

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 var express=require('express');
-
 var app=express();
-app.get('/',(req,res)=>{
-    res.send("hello");
+var mongoose= require('mongoose');
+var config=require('./config/database');
+var path=require('path');
+
+mongoose.Promise=global.Promise;
+mongoose.connect(config.uri,(err)=>{
+    if(err){
+        console.log('Not connected  '+err);
+    }else{
+        console.log('Connected')
+    }
 });
+
+app.use(express.static(__dirname+ '/client/dist/'));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname+ '/client/dist/index.html'));
+});
+
+
 app.listen(8080,()=>{
     console.log("running on port 8080");
 });
